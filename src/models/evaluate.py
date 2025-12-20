700a2788b9aacdfe22f44c3335fdaeb358945bf5
shubhu1026 <shubhu.p2000@gmail.com>
2025-12-20 02:46:56 -0500

    Modularizing EDA code into python scripts

---
+++ src/models/evaluate.py
@@ -0,0 +1,17 @@
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+def evaluate_model(model, X_test, y_test):
+    """
+    Evaluates a XGBoost Model on test_data.
+
+    Args:
+        model: Trained Model.
+        X_test: Test features.
+        y_test: Test labels.
+    """
+    proba = model.predict_proba(X_test)[:, 1]
+    threshold = 0.3
+    preds = (proba >= threshold).astype(int)
+    print("Classification Report:\n", classification_report(y_test, preds))
+    print("Confusion Matrix:\n", confusion_matrix(y_test, preds))
