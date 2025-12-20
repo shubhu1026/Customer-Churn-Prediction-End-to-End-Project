364282f3d75261212c2eae43dbab0ec4ff6b1067
shubhu1026 <shubhu.p2000@gmail.com>
2025-12-20 02:47:41 -0500

    added evalute.py

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -10,7 +10,7 @@ def evaluate_model(model, X_test, y_test):
         X_test: Test features.
         y_test: Test labels.
     """
-    proba = model.predict_proba(X_test)[:, 1]
+    proba = model.predict_proba(X_test)[:, 1]   
     threshold = 0.3
     preds = (proba >= threshold).astype(int)
     print("Classification Report:\n", classification_report(y_test, preds))
